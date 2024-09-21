14458e3595a6720af0427c299fb5d63c25b5aad0
splasherzz <juliasabado30@gmail.com>
2024-09-21 18:14:13 +0800

    style: define custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mastodon: '#5945D9', //purple
+        bluesky: '#0085FF', // blue
+        blackGreen: '#162721', // background
+        forestGreen: '#233732', // fields 
+        mintGreen: '#98CDC4', // accent text
+        slateGreen: '#2E524C', // dividers
+        teal: '#30554F', // timeline (posts/search bar)
+      },
+    },
   },
   plugins: [],
 }
