9327f2699b40cd51cb00dab90e9b8d5eafacc392
splasherzz <juliasabado30@gmail.com>
2024-10-07 21:19:21 +0800

    feat: make initial search bar component

---
+++ src/components/searchBar.svelte
@@ -0,0 +1,31 @@
+<script>
+    import FaSearch from 'svelte-icons/fa/FaSearch.svelte'
+</script>
+
+<div class="flex flex-col items-center justify-center">
+    <div class="relative my-2">
+        <div
+            class="icon absolute left-3.5 top-1/2 transform -translate-y-1/2"
+        >
+            <FaSearch />
+        </div>
+    <input
+        type="text"
+        class="search-bar pl-12 px-4 bg-teal text-lg text-mintGreen placeholder-mintGreen placeholder-opacity-50 hover:border border-mintGreen rounded-lg"
+        placeholder="Search"
+    />
+    </div>
+</div>
+
+<style>
+    .icon {
+        color: #98cdc4;
+        width: 20px;
+        height: 20px;
+    }
+
+    .search-bar {
+        width: 758px;
+        height: 48px;
+    }
+</style>
---
+++ src/routes/dashboard/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+    import SearchBar from '../../components/searchBar.svelte';
+</script>
+
+<div class="flex items-center justify-center h-screen">
+    <SearchBar />
+</div>
