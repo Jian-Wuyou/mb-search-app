8ad70e1512d0dfed38f0f0f0f10b9a32435516a7
KarlVerdad <karlfizz1999@gmail.com>
2024-10-08 16:22:16 +0800

    Added containers for the Side Bar and Timeline

--- src/components/searchBar.svelte
+++ src/components/searchBar.svelte
@@ -2,19 +2,15 @@
     import FaSearch from 'svelte-icons/fa/FaSearch.svelte'
 </script>
 
-<div class="flex flex-col items-center justify-center">
-    <div class="relative my-2">
-        <div
-            class="icon absolute left-3.5 top-1/2 transform -translate-y-1/2"
-        >
-            <FaSearch />
-        </div>
+<div class="search-bar">
+    <div class="icon absolute left-3.5 top-1/2 transform -translate-y-1/2">
+        <FaSearch />
+    </div>
     <input
         type="text"
-        class="search-bar pl-12 px-4 bg-teal text-lg text-mintGreen placeholder-mintGreen placeholder-opacity-50 hover:border border-mintGreen rounded-lg"
+        class="search-input pl-12 px-4 bg-teal text-lg text-mintGreen placeholder-mintGreen placeholder-opacity-50 hover:border border-mintGreen rounded-lg"
         placeholder="Search"
     />
-    </div>
 </div>
 
 <style>
@@ -25,7 +21,13 @@
     }
 
     .search-bar {
-        width: 758px;
+        position: relative;
         height: 48px;
+        width: 100%;
+    }
+
+    .search-input {
+        height: 100%;
+        width: 100%;
     }
 </style>
--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,7 +1,79 @@
 <script>
     import SearchBar from '../../components/searchBar.svelte';
+    import SideBar from '../../components/sideBar.svelte';
 </script>
 
-<div class="flex items-center justify-center h-screen">
-    <SearchBar />
+
+<div class="flex justify-center h-screen" style="overflow: auto;">
+    <div class="timeline-container">
+        <div class="search-container flex items-center">
+            <SearchBar />
+        </div>
+        <h1 style="text-align: center"><strong>Lorem</strong></h1>
+        <h1 style="text-align: center">Lorem</h1>
+        <h1 style="text-align: center">Lorem</h1>
+        <h1 style="text-align: center">Lorem</h1>
+        <h1 style="text-align: center">Lorem</h1>
+        <h1 style="text-align: center">Lorem</h1>
+        <h1 style="text-align: center">Lorem</h1>
+        <h1 style="text-align: center">Lorem</h1>
+        <h1 style="text-align: center">Lorem</h1>
+        <h1 style="text-align: center">Lorem</h1>
+        <h1 style="text-align: center">Lorem</h1>
+        <h1 style="text-align: center">Lorem</h1>
+        <h1 style="text-align: center">Lorem</h1>
+        <h1 style="text-align: center">Lorem</h1>
+        <h1 style="text-align: center">Lorem</h1>
+        <h1 style="text-align: center">Lorem</h1>
+        <h1 style="text-align: center">Lorem</h1>
+        <h1 style="text-align: center">Lorem</h1>
+        <h1 style="text-align: center">Lorem</h1>
+        <h1 style="text-align: center">Lorem</h1>
+        <h1 style="text-align: center">Lorem</h1>
+        <h1 style="text-align: center">Lorem</h1>
+        <h1 style="text-align: center">Lorem</h1>
+        <h1 style="text-align: center">Lorem</h1>
+        <h1 style="text-align: center">Lorem</h1>
+        <h1 style="text-align: center">Lorem</h1>
+        <h1 style="text-align: center">Lorem</h1>
+        <h1 style="text-align: center">Lorem</h1>
+        <h1 style="text-align: center">Lorem</h1>
+        <h1 style="text-align: center">Lorem</h1>
+        <h1 style="text-align: center">Lorem</h1>
+        <h1 style="text-align: center">Lorem</h1>
+        <h1 style="text-align: center">Lorem</h1>
+        <h1 style="text-align: center">Lorem</h1>
+        <h1 style="text-align: center">Lorem</h1>
+        <h1 style="text-align: center">Lorem</h1>
+        <h1 style="text-align: center">Lorem</h1>
+        <h1 style="text-align: center">Lorem</h1>
+        <h1 style="text-align: center">Lorem</h1>
+        <h1 style="text-align: center">Lorem</h1>
+        <h1 style="text-align: center">Lorem</h1>
+        <h1 style="text-align: center">Lorem</h1>
+        <h1 style="text-align: center">Lorem</h1>
+        <h1 style="text-align: center">Lorem</h1>
+        <h1 style="text-align: center">Lorem</h1>
+    </div>
+
+    <SideBar />
 </div>
+
+<style>
+    .timeline-container {
+        height: fit-content;
+        border-left: 1px solid #2E524C;
+        border-right: 1px solid #2E524C;
+    }
+
+    .search-container {
+        position: sticky;
+        top: 0;
+        width: 758px;
+        height: 120px;
+        padding: 2em;
+        border-bottom: 1px solid #2E524C;
+        background-color: #162721;
+    }
+
+</style>
