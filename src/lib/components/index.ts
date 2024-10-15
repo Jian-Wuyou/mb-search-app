460c666fe4dfbb03d88819b2d68dc306870810d2
Jian-Wuyou <dramante@up.edu.ph>
2024-10-15 16:35:51 +0800

    Fix index.ts

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -1,6 +1,6 @@
 export { default as LoginForm } from './LoginForm.svelte';
-export { default as SearchBar } from './searchBar.svelte';
-export { default as SideBar } from './sideBar.svelte';
-export { default as TimelinePost } from './timelinePost.svelte';
-export { default as PostContent } from './postContent.svelte';
-export { default as Status } from './status.svelte';
+export { default as SearchBar } from './SearchBar.svelte';
+export { default as SideBar } from './SideBar.svelte';
+export { default as TimelinePost } from './TimelinePost.svelte';
+export { default as PostContent } from './PostContent.svelte';
+export { default as Status } from './Status.svelte';
