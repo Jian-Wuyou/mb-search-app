b0b742022693aecd873dc7d14affcb4bdf9f3e4e
Jian-Wuyou <dramante@up.edu.ph>
2024-10-22 16:21:49 +0800

    fix: incorrect use of svelte store

--- src/lib/components/SideBar.svelte
+++ src/lib/components/SideBar.svelte
@@ -6,12 +6,12 @@
 
     async function logout_mastodon() {
         await sessionStore.remove_mastodon();
-        $mastodon_posts.set([]);
+        mastodon_posts.set([]);
     }
 
     async function logout_bluesky() {
         await sessionStore.remove_bluesky();
-        $bluesky_posts.set([]);
+        bluesky_posts.set([]);
     }
 
 </script>
